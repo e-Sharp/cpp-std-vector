8acbf80ea9b22aca310478c2c39ec25f103117b7
Willy | e-Sharp <willy.esharp@gmail.com>
2020-02-05 23:15:59 +0100

    removed constexpr for C++17 compat

--- src/vector.hpp
+++ src/vector.hpp
@@ -18,88 +18,88 @@ public:
 	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
 	// [vector.cons], construct/copy/destroy
-	constexpr vector() noexcept(noexcept(Allocator())) : vector(Allocator()) { }
-	constexpr explicit vector(const Allocator&) noexcept;
-	constexpr explicit vector(size_type n, const Allocator& = Allocator());
-	constexpr vector(size_type n, const T& value, const Allocator& = Allocator());
+	vector() noexcept(noexcept(Allocator())) : vector(Allocator()) { }
+	explicit vector(const Allocator&) noexcept;
+	explicit vector(size_type n, const Allocator& = Allocator());
+	vector(size_type n, const T& value, const Allocator& = Allocator());
 	template<class InputIterator>
-	  constexpr vector(InputIterator first, InputIterator last, const Allocator& = Allocator());
-	constexpr vector(const vector& x);
-	constexpr vector(vector&&) noexcept;
-	constexpr vector(const vector&, const Allocator&);
-	constexpr vector(vector&&, const Allocator&);
-	constexpr vector(initializer_list<T>, const Allocator& = Allocator());
-	constexpr ~vector();
-	constexpr vector& operator=(const vector& x);
-	constexpr vector& operator=(vector&& x)
+	  vector(InputIterator first, InputIterator last, const Allocator& = Allocator());
+	vector(const vector& x);
+	vector(vector&&) noexcept;
+	vector(const vector&, const Allocator&);
+	vector(vector&&, const Allocator&);
+	vector(initializer_list<T>, const Allocator& = Allocator());
+	~vector();
+	vector& operator=(const vector& x);
+	vector& operator=(vector&& x)
 	  noexcept(allocator_traits<Allocator>::propagate_on_container_move_assignment::value ||
 			   allocator_traits<Allocator>::is_always_equal::value);
-	constexpr vector& operator=(initializer_list<T>);
+	vector& operator=(initializer_list<T>);
 	template<class InputIterator>
-	  constexpr void assign(InputIterator first, InputIterator last);
-	constexpr void assign(size_type n, const T& u);
-	constexpr void assign(initializer_list<T>);
-	constexpr allocator_type get_allocator() const noexcept;
+	  void assign(InputIterator first, InputIterator last);
+	void assign(size_type n, const T& u);
+	void assign(initializer_list<T>);
+	allocator_type get_allocator() const noexcept;
 
 	// iterators
-	constexpr iterator               begin() noexcept;
-	constexpr const_iterator         begin() const noexcept;
-	constexpr iterator               end() noexcept;
-	constexpr const_iterator         end() const noexcept;
-	constexpr reverse_iterator       rbegin() noexcept;
-	constexpr const_reverse_iterator rbegin() const noexcept;
-	constexpr reverse_iterator       rend() noexcept;
-	constexpr const_reverse_iterator rend() const noexcept;
+	iterator               begin() noexcept;
+	const_iterator         begin() const noexcept;
+	iterator               end() noexcept;
+	const_iterator         end() const noexcept;
+	reverse_iterator       rbegin() noexcept;
+	const_reverse_iterator rbegin() const noexcept;
+	reverse_iterator       rend() noexcept;
+	const_reverse_iterator rend() const noexcept;
 
-	constexpr const_iterator         cbegin() const noexcept;
-	constexpr const_iterator         cend() const noexcept;
-	constexpr const_reverse_iterator crbegin() const noexcept;
-	constexpr const_reverse_iterator crend() const noexcept;
+	const_iterator         cbegin() const noexcept;
+	const_iterator         cend() const noexcept;
+	const_reverse_iterator crbegin() const noexcept;
+	const_reverse_iterator crend() const noexcept;
 
 	// [vector.capacity], capacity
-	[[nodiscard]] constexpr bool empty() const noexcept;
-	constexpr size_type size() const noexcept;
-	constexpr size_type max_size() const noexcept;
-	constexpr size_type capacity() const noexcept;
-	constexpr void      resize(size_type sz);
-	constexpr void      resize(size_type sz, const T& c);
-	constexpr void      reserve(size_type n);
-	constexpr void      shrink_to_fit();
+	[[nodiscard]] bool empty() const noexcept;
+	size_type size() const noexcept;
+	size_type max_size() const noexcept;
+	size_type capacity() const noexcept;
+	void      resize(size_type sz);
+	void      resize(size_type sz, const T& c);
+	void      reserve(size_type n);
+	void      shrink_to_fit();
 
 	// element access
-	constexpr reference       operator[](size_type n);
-	constexpr const_reference operator[](size_type n) const;
-	constexpr const_reference at(size_type n) const;
-	constexpr reference       at(size_type n);
-	constexpr reference       front();
-	constexpr const_reference front() const;
-	constexpr reference       back();
-	constexpr const_reference back() const;
+	reference       operator[](size_type n);
+	const_reference operator[](size_type n) const;
+	const_reference at(size_type n) const;
+	reference       at(size_type n);
+	reference       front();
+	const_reference front() const;
+	reference       back();
+	const_reference back() const;
 
 	// [vector.data], data access
-	constexpr T*       data() noexcept;
-	constexpr const T* data() const noexcept;
+	T*       data() noexcept;
+	const T* data() const noexcept;
 
 	// [vector.modifiers], modifiers
-	template<class... Args> constexpr reference emplace_back(Args&&... args);
-	constexpr void push_back(const T& x);
-	constexpr void push_back(T&& x);
-	constexpr void pop_back();
+	template<class... Args> reference emplace_back(Args&&... args);
+	void push_back(const T& x);
+	void push_back(T&& x);
+	void pop_back();
 
-	template<class... Args> constexpr iterator emplace(const_iterator position, Args&&... args);
-	constexpr iterator insert(const_iterator position, const T& x);
-	constexpr iterator insert(const_iterator position, T&& x);
-	constexpr iterator insert(const_iterator position, size_type n, const T& x);
+	template<class... Args> iterator emplace(const_iterator position, Args&&... args);
+	iterator insert(const_iterator position, const T& x);
+	iterator insert(const_iterator position, T&& x);
+	iterator insert(const_iterator position, size_type n, const T& x);
 	template<class InputIterator>
-	  constexpr iterator insert(const_iterator position,
+	  iterator insert(const_iterator position,
 								InputIterator first, InputIterator last);
-	constexpr iterator insert(const_iterator position, initializer_list<T> il);
-	constexpr iterator erase(const_iterator position);
-	constexpr iterator erase(const_iterator first, const_iterator last);
-	constexpr void     swap(vector&)
+	iterator insert(const_iterator position, initializer_list<T> il);
+	iterator erase(const_iterator position);
+	iterator erase(const_iterator first, const_iterator last);
+	void     swap(vector&)
 	  noexcept(allocator_traits<Allocator>::propagate_on_container_swap::value ||
 			   allocator_traits<Allocator>::is_always_equal::value);
-	constexpr void     clear() noexcept;
+	void     clear() noexcept;
 };
 
 template<class InputIterator, class Allocator = allocator<iter-value-type<InputIterator>>>
@@ -108,5 +108,5 @@ vector(InputIterator, InputIterator, Allocator = Allocator())
 
 // swap
 template<class T, class Allocator>
-constexpr void swap(vector<T, Allocator>& x, vector<T, Allocator>& y)
+void swap(vector<T, Allocator>& x, vector<T, Allocator>& y)
 noexcept(noexcept(x.swap(y)));
