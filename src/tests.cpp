bf7911888480d09adce01fcfeafa0ed37c32c745
Willy | e-Sharp <willy.esharp@gmail.com>
2020-02-12 13:57:00 +0100

    test default ctor

--- src/tests.cpp
+++ src/tests.cpp
@@ -2,3 +2,10 @@
 #include <Catch2/catch.hpp>
 
 #include "vector.hpp"
+
+TEST_CASE("vectors can be default constructed") {
+    auto v = vector<int>{};
+
+    REQUIRE(v.capacity() == 0);
+    REQUIRE(v.size() == 0);
+}
